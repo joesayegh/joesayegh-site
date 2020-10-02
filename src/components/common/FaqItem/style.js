dd9011f12a24a0979a7997271676e47a15393ad6
Joseph Sayegh <joesayegh@gmail.com>
2020-10-02 22:17:04 +0100

    decreases font-size of faq title, content, removes padding and update position of arrows

--- src/components/common/FaqItem/style.js
+++ src/components/common/FaqItem/style.js
@@ -19,14 +19,19 @@ export const Wrapper = styled.div`
     text-align: left;
     cursor: pointer;
     color: var(--text);
-    // font-family: ${props => props.theme.font.secondary};
-    ${props => props.theme.font_size.regular};
-    // font-weight: bold;
+    font-size: 1rem;
+    line-height: 1;
     padding: 24px;
     padding-right: 72px;
-    padding-left: 16px;
+    padding-left: 0;
     position: relative;
 
+    @media (min-width: ${props => props.theme.screen.sm}) {
+      font-size: 1.2rem;
+      line-height: 1;
+    }
+
+    // THIS IS THE ARROW
     &::after {
       content: '';
       display: block;
@@ -34,13 +39,16 @@ export const Wrapper = styled.div`
       height: 10px;
       border-left: 2px solid var(--text);
       border-bottom: 2px solid var(--text);
-      // border-left: 2px solid ${props => props.theme.color.black.regular};
-      // border-bottom: 2px solid ${props => props.theme.color.black.regular};
       position: absolute;
-      top: 34px;
-      right: 36px;
+      top: 26px;
+      right: 26px;
       transform: rotate(-45deg);
       transition: transform 0.3s ease-in-out;
+
+      @media (min-width: ${props => props.theme.screen.sm}) {
+        top: 32px;
+        right: 32px;
+      }
     }
 
     &.active {
@@ -51,10 +59,20 @@ export const Wrapper = styled.div`
   }
 
   .faq-content {
-    padding: 0 72px 32px 16px;
-    line-height: 26px;
-    ${props => props.theme.font_size.small};
+    padding: 0 0 32px 0;
     color: var(--text);
-    // color: ${props => props.theme.color.black.light};
+    font-size: 0.9rem;
+    line-height: 1.4;
+
+    @media (min-width: ${props => props.theme.screen.sm}) {
+      font-size: 1rem;
+      line-height: 1.4;
+      padding: 0 72px 32px 0;
+    }
+
+    a {
+      color: inherit;
+    }
+
   }
 `;
