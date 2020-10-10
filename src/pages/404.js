518fea11fc65ef3438378ffb1f94aac6e516279d
Joseph Sayegh <joesayegh@gmail.com>
2020-10-10 17:53:50 +0100

    updates 404 page styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,19 +4,19 @@ import React from 'react';
 import Layout from '@common/Layout';
 import Navbar from '@common/Navbar';
 
-import About from '@sections/About';
 import Footer from '@sections/Footer';
 
-import { Container } from '@components/global';
+import { Section, Container } from '@components/global';
 
 const NotFoundPage = () => (
   <Layout>
     <Navbar />
-    <About />
-    <Container>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Container>
+    <Section style={{ background: 'var(--bg)', color: 'var(--text)' }}>
+      <Container>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Container>
+    </Section>
     <Footer />
   </Layout>
 );
